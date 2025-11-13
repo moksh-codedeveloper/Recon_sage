02248ab970ce145e5c030650b61466dbeeed9eab
Malde Moksh <maldemoksh437@gmail.com>
2025-11-13 18:01:07 +0530

    feat: update bening_request method to handle multiple domains for rate limit detection

--- core_scanner/rate_limiting.py
+++ core_scanner/rate_limiting.py
@@ -10,7 +10,15 @@ class RateLimitDetection:
     
     async def bening_request(self):
         semaphore = asyncio.Semaphore(self.concurrency)
+        domains = []
+        for paths_list in self.list_paths_to_test :
+            full_domain = self.url + paths_list
+            domains.append(full_domain)
+        print("Bening request are preparing to start firing the request on the targets......")
         async with semaphore:
             async with httpx.AsyncClient(timeout=self.timeout) as client:
-                response = await client.get(self.url)
-                
+                for domain  in domains :
+                    response = await client.get(domain)
+                    resp_headers = response.headers
+                    resp_status_code = response.status_code 
+                                   
