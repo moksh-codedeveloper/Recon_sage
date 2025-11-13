442526e420cdc481c7e305400803c645ffb70f3c
Malde Moksh <maldemoksh437@gmail.com>
2025-11-13 20:59:29 +0530

    refactor: reorganize bening_request method for improved domain handling and response processing

--- core_scanner/rate_limiting.py
+++ core_scanner/rate_limiting.py
@@ -1,4 +1,6 @@
 import asyncio
+import datetime
+import hashlib
 import httpx
 class RateLimitDetection:
     def __init__(self, target, custom_headers, list_paths_to_test, timeout, concurrency):
@@ -8,17 +10,34 @@ class RateLimitDetection:
         self.timeout = timeout
         self.concurrency = concurrency
     
+    def hash_snippet(self, body, length=300):
+        """Hash only first N bytes/chars depending on input type."""
+        if isinstance(body, bytes):                # bytes → raw
+            snippet = body[:length]
+            return hashlib.sha256(snippet).hexdigest()
+        else:                                      # str → encode
+            snippet = body[:length]
+            return hashlib.sha256(snippet.encode()).hexdigest()
+
     async def bening_request(self):
-        semaphore = asyncio.Semaphore(self.concurrency)
-        domains = []
-        for paths_list in self.list_paths_to_test :
-            full_domain = self.url + paths_list
-            domains.append(full_domain)
-        print("Bening request are preparing to start firing the request on the targets......")
-        async with semaphore:
-            async with httpx.AsyncClient(timeout=self.timeout) as client:
-                for domain  in domains :
-                    response = await client.get(domain)
-                    resp_headers = response.headers
-                    resp_status_code = response.status_code 
-                                   
+        all_domain = []
+        for domain in self.list_paths_to_test :
+            s = domain.strip()
+            if s :
+                target = self.url + domain
+                all_domain.append(target)
+        result = {}
+        async with httpx.AsyncClient(timeout=self.timeout) as client:
+            for domain in all_domain:
+                response = await client.get(domain)
+                
+                status_code = response.status_code
+                headers = response.headers
+                response_text = response.text 
+                response_bytes = response.content 
+                
+                hashed_text = self.hash_snippet(response_text)
+                hashed_bytes =  self.hash_snippet(response_bytes)
+                
+                timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%D-%M-%Y")
+                result[timestamp]
