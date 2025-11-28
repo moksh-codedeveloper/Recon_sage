c320eb7bcd1cd50ce1b7dc280a6acce9df6bcdb5
Malde Moksh <maldemoksh437@gmail.com>
2025-11-28 22:11:01 +0530

    feat: implement RateLimitDetector class with async scan_batch method for improved rate limit detection

--- core_scanner/rate_limiting.py
+++ core_scanner/rate_limiting.py
@@ -1,12 +1,35 @@
-import asyncio
-from target_fingerprinting import PassiveFingerprint
-class RateLimitDetection:
-    def __init__(self, target, custom_headers, list_paths_to_test, timeout, concurrency):
-        self.url = target
-        self.custom_headers = custom_headers
-        self.list_paths_to_test = list_paths_to_test
+from core_scanner.target_fingerprinting import PassiveFingerprint
+import asyncio 
+
+class RateLimitDetector:
+    def __init__(self, target, list_dirs, concurrency, timeout):
+        self.target = target
+        self.list_of_dirs = list_dirs
         self.timeout = timeout
         self.concurrency = concurrency
-    
-    def benign_request(self, timeout, concurrency):
-        pf = PassiveFingerprint(target=self.url, timeout=timeout, concurrency=concurrency)
+
+    async def scan_batch(self):
+        batch = []
+        try:
+            pf = PassiveFingerprint(target=self.target, concurrency=self.concurrency, timeout=self.timeout)
+            sem = asyncio.Semaphore(self.concurrency)
+            async def resp_ext(domains) :
+                async with sem:
+                    scan_result = await pf.scan_data(domain=domains)
+                    return scan_result
+            tasks = [resp_ext(d) for d in self.list_of_dirs]
+            all_result = await asyncio.gather(*tasks)
+            await pf.close()
+            for results in all_result:
+                batch.append((
+                    results["status_code"],
+                    results["latency_ms"],
+                    results["headers"].get("Retry-After"),
+                    results["headers"].get("X-RateLimit-Remaining")
+                ))
+            return batch
+        except Exception as e:
+          print(f"One Exception occured here while scanning its the rate limits scan block {e}")
+          return []
+    def detect_rate_limit_from_batch(self, batch):
+        pass
