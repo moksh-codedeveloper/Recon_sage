bade84e4e04d15f851a813cbfc21762790116e37
Malde Moksh <maldemoksh437@gmail.com>
2025-12-05 21:27:51 +0530

    Improved and Modified the structure of the code

--- waf_scanner_module/waf_detection.py
+++ waf_scanner_module/waf_detection.py
@@ -112,17 +112,37 @@ class WafDetection:
                 "error_message" : f"The error reason message is this  :- {e}",
                 "status_code" : 0
             }
-    def check_for_cloudflare(self, headers:dict):
-        cloudflare_headers = ['cf-ray', 'cf-cache-status', 'cf-request-id', 'cf-connecting-ip', 'cf-ipcountry', 'cf-warp-tag-id', 'cf-bgj']
-        advanced_cloudflare_headers = ['cf-chl', 'cf-chl-bypasses', 'cf-chl-out', 'cf-mitigated', 'cf-turnstile', 'cf-challenge']
-        all_match_headers = {}
-        for key, value in headers.items():
+    def check_for_cloudflare(self, headers:dict): # This one are not same they both are different (headers != all_matched_headers)
+        cloudflare_headers = [
+            'cf-ray', 
+            'cf-cache-status', 
+            'cf-request-id', 
+            'cf-connecting-ip', 
+            'cf-ipcountry', 
+            'cf-warp-tag-id', 
+            'cf-bgj'
+            ]
+        
+        advanced_cloudflare_headers = [
+            'cf-chl', 
+            'cf-chl-bypasses', 
+            'cf-chl-out', 
+            'cf-mitigated', 
+            'cf-turnstile', 
+            'cf-challenge'
+            ]
+        
+        all_match_headers = {} 
+        
+        for key, value in headers.items(): 
             if key in cloudflare_headers or key in advanced_cloudflare_headers:
                 all_match_headers[key] = value
             if key == "server" and "cloudflare" in str(value).lower():
                 all_match_headers[key] = value
+        
         return {
             "message" : "This are some headers key which matched with there values",
+            "is_it_cloudflare_waf" : True if all_match_headers != {} else False,
             "matched_headers" : all_match_headers 
         }
     
@@ -146,7 +166,7 @@ class WafDetection:
         
         return {
             "message" : "You have this result of the headers if this is present or empty",
-            "is_it_waf" : True if all_matched_headers != None or all_matched_headers != {} else False,
+            "is_it_fastly_waf" : True if all_matched_headers != {} else False,
             "headers" : all_matched_headers
         }
     
@@ -158,25 +178,28 @@ class WafDetection:
             'x-akamai-device-characteristics', 
             'x-true-cache-key', 
             'x-check-cacheable'
-            ] # server : "AkamaiGhost"
+        ]
+        
         imperva_headers = [
             'x-iinfo', 
             'x-cdn', 
             'x-incapsula', 
             'x-cdn-request-id'
-            ] # via : 1.1 incapsula and x-frame-options : SAMEORIGIN and X-CDN : Imperva
+        ]
+        
         aws_headers = [
             'x-amz-cf-id',
             'x-amz-cf-pop',
             'x-amz-cf-paired-pop',
             'x-amzn-trace-id',
-            'x-amzn-RequestId',
-            'x-amzn-ErrorType'
+            'x-amzn-requestid',
+            'x-amzn-errortype'
         ]
         
         all_headers_matched_akamai = {}
         all_headers_mached_imperva = {}
         all_headers_matched_aws = {}
+        
         for key, values in headers.items():
             if key in imperva_headers:
                 all_headers_mached_imperva[key] = values
@@ -184,16 +207,17 @@ class WafDetection:
             if key == 'x-cdn' and 'imperva' in str(values).lower():
                 all_headers_mached_imperva[key] = values
             
-            if key == 'via' and '1.1 incapsula' in str(values).lower():
+            if key == 'via' and 'incapsula' in str(values).lower():
                 all_headers_mached_imperva[key] = values
             
-            if key == 'x-frame-options' and 'sameorigin' in str(values).lower():
-                all_headers_mached_imperva[key] = values
             
             if key in akamai_kona_headers:
                 all_headers_matched_akamai[key] = values
             
             if key == "server" and "akamaighost" in str(values).lower():
+                all_headers_matched_akamai[key] = values
+             
+            if key == "via" and "akamai" in str(values).lower():
                 all_headers_matched_akamai[key] = values 
             
             if key in aws_headers:
@@ -201,9 +225,9 @@ class WafDetection:
             
         return {
             "message" : "You have the summary of result of here in below" ,
-            "is_it_waf_related_to_imperva" : True if all_headers_mached_imperva != None or all_headers_mached_imperva != {} else False,
-            "is_it_waf_related_to_aws" : True if all_headers_matched_aws != None or all_headers_matched_aws != {} else False,
-            "is_it_waf_related_to_akamai" : True if all_headers_matched_akamai != None or all_headers_matched_akamai != {} else False,
+            "is_it_waf_related_to_imperva" : True if all_headers_mached_imperva != {} else False,
+            "is_it_waf_related_to_aws" : True if all_headers_matched_aws != {} else False,
+            "is_it_waf_related_to_akamai" : True if all_headers_matched_akamai != {} else False,
             "akamai_headers" : all_headers_matched_akamai,
             "aws_headers" : all_headers_matched_aws,
             "imperva_headers" : all_headers_mached_imperva
